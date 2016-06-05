6ae4ce24db15c71a76637c4309f00d49c152d7a2
root <root@server3.escovan.net>
2016-06-05 03:27:10 +0200

    Logged.be First commit

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+escovan:$1$Br7a5E1E$Z/JYHuPKi36HPslErHMHc/:Escovan Hacktics:escovan@escovan.net:admin,user
